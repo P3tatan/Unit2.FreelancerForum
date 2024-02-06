c1276f9335363cd667f25ae866b3841ff90276c0
Ester Petatan <126705109+P3tatan@users.noreply.github.com>
2024-02-05 19:07:13 -0500

    Update W.19.js

--- W.19.js
+++ W.19.js
@@ -1,1 +1,24 @@
+// Users story:
 
+// When a visitor enters a website and finds a list of available freelancers. Each freelancer  has a name, and occupation, and a starting price for their services. 
+
+// A visitor finds a messgage that displays the average starting price of all freelancers.
+
+// The average starting price is $40
+
+// For Carol she is a freelancer and new listing appears. She is a programmer who's starting price is $70.
+
+// The average price is starting is updated $50
+
+// Remeber REACTO.
+
+Keep this in mind:
+Is your HTML file connected to your JS file?
+Have you defined arrays for possible names and occupations?
+Have you defined an initial array of freelancers?
+Do you know how you want the freelancers' information to be displayed on the page?
+Have you written and called a function to render the initial freelancer data?
+Have you written a function to generate a new random freelancer?
+Is this function being called in an interval?
+Have you written a function to calculate the average starting price of your freelancers' array?
+When should this function be called to update the displayed message?
